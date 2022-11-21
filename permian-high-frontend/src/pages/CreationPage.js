e0be2cde0551652922d07f2d489aec1bbad3b140
Justin Junious <justinjunious@gmail.com>
2022-11-21 14:18:15 -0500

    added items to creation page from students page

--- permian-high-frontend/src/pages/CreationPage.js
+++ permian-high-frontend/src/pages/CreationPage.js
@@ -4,6 +4,11 @@ import { BASE_URL } from '../services/api'
 
 const CreationPage = () => {
   const [grade, setGrade] = useState([])
+  const [courses, setcourses] = useState([])
+  const [students, setStudents] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const [courseId, setCourseId] = useState()
+  const [search, setSearch] = useState()
   const initialFormState = {
     name: '',
     grade: ''
@@ -28,6 +33,35 @@ const CreationPage = () => {
   useEffect(() => {
     GetGrades()
   }, [])
+
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await axios.get(`${BASE_URL}courses`)
+      setcourses(response.data)
+    }
+    apiCall()
+  }, [])
+
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await axios.get(`${BASE_URL}students`)
+      setStudents(response.data)
+    }
+    apiCall()
+  }, [])
+
+  const handleChange2 = (evt) => {
+    setCourseId({ studentId: evt.target.value })
+  }
+  const handleId = (evt) => {
+    setSearch(evt.target.value)
+  }
+
+  const handleSubmit2 = async (evt) => {
+    evt.preventDefault()
+    let response = await axios.get(`${BASE_URL}courses/${courseId}`, search)
+    setSearchResults(response.data)
+  }
 
   return (
     <div className="course-form-container">
@@ -61,6 +95,34 @@ const CreationPage = () => {
           ))}
         </select>
       </form>
+      <div>
+        {courses.map((course) => (
+          <ul key={course.id}>
+            <h2>{course.name}</h2>
+            <h3>{course.students.name}</h3>
+            <h4>{course.result.score}</h4>
+          </ul>
+        ))}
+        <form onSubmit={handleSubmit2}>
+          <select id="course" onChange={handleId}>
+            <option></option>
+            {courses.map((course) => (
+              <option value={course.id} key={course.id}>
+                {course.name}
+              </option>
+            ))}
+          </select>
+          <select id="students" onChange={handleChange2}>
+            <option></option>
+            {students.map((student) => (
+              <option value={student.id} key={student.id}>
+                {student.name}
+              </option>
+            ))}
+          </select>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
