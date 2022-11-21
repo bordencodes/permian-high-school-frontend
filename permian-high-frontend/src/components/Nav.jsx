f2373730b263354d2c52cb8f5e4a4d65029f297e
Bret Borden <bordenb21@gmail.com>
2022-11-21 16:06:10 -0500

    styled nav bar

--- permian-high-frontend/src/components/Nav.jsx
+++ permian-high-frontend/src/components/Nav.jsx
@@ -2,12 +2,12 @@ import {Link} from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <nav className='navbar'>
-      <h4>Permian High</h4>
-      <div>
-        <Link to='/'><li>Home</li></Link>
-        <Link to='/students'><li>Students</li></Link>
-        <Link to='/courses'><li>Courses</li></Link>
+    <nav>
+      <div className='header'>Permian High School</div>
+      <div className='navbar'>
+        <Link to='/'>Home</Link>
+        <Link to='/students'>Students</Link>
+        <Link to='/courses'>Courses</Link>
       </div>
     </nav>
   )
