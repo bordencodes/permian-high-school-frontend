a71a14526cf7bd2983a81a91aba821e267cde9d3
Justin Junious <justinjunious@gmail.com>
2022-11-21 13:58:26 -0500

    added content to students page

--- permian-high-frontend/src/pages/students.js
+++ permian-high-frontend/src/pages/students.js
@@ -1,10 +1,15 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { BASE_URL } from '../services/api'
+import { useParams } from 'react-router-dom'
 
 const Students = () => {
   const [courses, setcourses] = useState([])
   const [students, setStudents] = useState([])
+  const [searchResults, setSearchResults] = useState([])
+  const [courseId, setCourseId] = useState()
+  const [search, setSearch] = useState()
+  const { id } = useParams()
 
   useEffect(() => {
     const apiCall = async () => {
@@ -22,6 +27,13 @@ const Students = () => {
     apiCall()
   }, [])
 
+  const handleChange = (evt) => {
+    setCourseId({ studentId: evt.target.value })
+  }
+  const handleChange = (evt) => {
+    setSearch({ studentId: evt.target.value })
+  }
+
   return (
     <div>
       Hello
@@ -35,8 +47,15 @@ const Students = () => {
         ))}
 
         <form>
-          <input></input>
-          <select id="students">
+          <select id="course" onChange={}>
+            <option></option>
+            {courses.map((course) => (
+              <option value={course.id} key={course.id}>
+                {course.name}
+              </option>
+            ))}
+          </select>
+          <select id="students" onChange={handleChange}>
             <option></option>
             {students.map((student) => (
               <option value={student.id} key={student.id}>
@@ -44,6 +63,7 @@ const Students = () => {
               </option>
             ))}
           </select>
+          <button>Submit</button>
         </form>
       </div>
     </div>
